fde3e3dd8f575dfdebeace4cd6f9509df82fb732
Yaosheng Yue <yyue@gtreasury.com>
2021-09-06 13:57:01 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> weekDays = new List<string>();
+            GetWeekDays(ref weekDays);
+            GetWeekDays2(ref weekDays);
+
+        }
+
+        static void GetWeekDays(ref List<string> weekDays)
+        {
+            weekDays.Add("Monday");
+        }
+
+        static void GetWeekDays2(ref List<string> weekDays)
+        {
+            weekDays.Add("Tuesday");
+        }
+    }
+}
